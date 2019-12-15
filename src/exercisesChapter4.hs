66b4f91997bd893f226b4acdc5e117bc7a0850bc
Jens Kouros <jens.kouros@breuninger.de>
2019-12-15 10:21:00 +0100

    Add comments in execisesChapter4.hs

---
+++ src/exercisesChapter4.hs
@@ -0,0 +1,36 @@
+module ExercisesChapter4 where
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome l =
+  (reverse l) == l
+
+myAbs :: Integer -> Integer
+myAbs x =
+  if x >= 0 then x else (-x)
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f (a, b) (c, d) =
+  ((b, d), (a, c))
+
+-- Correcting syntax
+{-
+1. Here, we want a function that adds 1 to the length of a string argument and returns that result.
+
+    x = (+)
+    F xs = w 'x' 1
+         where w = length xs
+-}
+
+x = (+)
+f2 xs = w `x` 1
+  where w = length xs
+
+
+{-
+3. When fixed, this function will return 1 from the value (1, 2).
+
+   f (a b) = A
+-}
+
+f3 (a, b) = a
+
