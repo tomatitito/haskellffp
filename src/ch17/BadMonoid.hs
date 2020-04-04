336eafdb005509fe475097f867382e9e5f2fc421
Jens Kouros <jens.kouros@breuninger.de>
2020-04-04 14:35:19 +0200

    Add module Bad Monoid from chapter 17

---
+++ src/ch17/BadMonoid.hs
@@ -0,0 +1,28 @@
+module BadMonoid where
+
+import Data.Monoid
+import Data.Semigroup
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+data Bull = Fools | Twoo deriving (Eq, Show)
+
+instance Arbitrary Bull where
+  arbitrary =
+    frequency [(1, return Fools)
+              ,(1, return Twoo)]
+
+instance Semigroup Bull where
+  (<>) = mappend
+  
+instance Monoid Bull where
+  mempty = Fools
+  mappend _ _ = Fools
+
+-- EqProp is from the checkers library
+instance EqProp Bull where
+  (=-=) = eq
+
+main:: IO ()
+main = quickBatch (monoid Twoo)
